26cce640497203c9324bca77511b3c32873e2b17
kira134679 <kira134679@gmail.com>
2025-09-19 00:16:00 +0800

    WIP

---
+++ assets/js/productDetail.js
@@ -0,0 +1,56 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+import '/assets/scss/product-detail.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// const swiper = new Swiper('.mySwiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     992: {
+
+//     }
+//   }
+
+// });
+
+// thumbnail
+const swiper = new Swiper('.mySwiper', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    className: 'swiper-pagination-bullet',
+    renderBullet: function (className) {
+      return `<span class="${className}"></span>`;
+    },
+  },
+});
+
+// main
+const swiper2 = new Swiper('.mySwiper2', {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
