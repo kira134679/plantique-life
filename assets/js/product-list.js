b4d5d2d720490cdd0774be0c1b777ef972a4dbd4
YuJu <yuju99924@gmail.com>
2025-09-29 20:15:33 +0800

    feat: add product-list-page

---
+++ assets/js/product-list.js
@@ -0,0 +1,9 @@
+import '/assets/scss/product-list.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 24,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
