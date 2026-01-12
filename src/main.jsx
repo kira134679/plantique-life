f01a91f1eebfca75e510ab2cb48558e68b1a935b
kira134679 <kira134679@gmail.com>
2026-01-12 22:42:59 +0800

    feat: add Toaster globally

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router';
 import router from './router/index';
@@ -11,6 +12,7 @@ import { store } from './store/store';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <Toaster />
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
