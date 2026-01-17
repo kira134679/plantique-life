acdc1a4af0b22d891c8c6af38cc1f06f05a72fab
kira134679 <kira134679@gmail.com>
2026-01-17 14:54:53 +0800

    feat(api): add product API module
    ^ Conflicts:
    ^	src/api/index.js
    ^	src/api/services/product.js

--- src/api/services/product.js
+++ src/api/services/product.js
@@ -1,5 +1,84 @@
 import adminClient from '../clients/adminClient';
-import { ADMIN_API_PATH } from '../core/constants';
+import guestClient from '../clients/guestClient';
+import { ADMIN_API_PATH, GUEST_API_PATH } from '../core/constants';
+
+export const productApi = {
+  /**
+   * 取得所有商品資料。
+   * @returns {Promise<{
+   *   success: boolean,
+   *   products: {
+   *     category: string,
+   *     content: string,
+   *     description: string,
+   *     id: string,
+   *     imageUrl: string,
+   *     imagesUrl: string[],
+   *     is_enabled: number,
+   *     origin_price: number,
+   *     price: number,
+   *     title: string,
+   *     unit: string
+   *   }[]
+   * }>}
+   */
+  getAllProducts: (config = {}) => guestClient.get(`${GUEST_API_PATH}/products/all`, config),
+  /**
+   * 取得商品資料，支援分頁與分類篩選。
+   * @param {object} params - 查詢參數
+   * @param {number} [params.page] - 欲取得的頁碼
+   * @param {string} [params.category] - 欲篩選的商品分類
+   * @returns {Promise<{
+   *   success: boolean,
+   *   products: {
+   *     category: string,
+   *     content: string,
+   *     description: string,
+   *     id: string,
+   *     imageUrl: string,
+   *     imagesUrl: string[],
+   *     is_enabled: number,
+   *     num: number,
+   *     origin_price: number,
+   *     price: number,
+   *     title: string,
+   *     unit: string
+   *   }[],
+   *   pagination: {
+   *     total_pages: number,
+   *     current_page: number,
+   *     has_pre: boolean,
+   *     has_next: boolean,
+   *     category: string,
+   *   }
+   *   messages: string[]
+   * }>}
+   */
+  getProducts: ({ preventGlobalLoading, ...params } = {}) => {
+    return guestClient.get(`${GUEST_API_PATH}/products`, { preventGlobalLoading, params });
+  },
+  /**
+   * 取得特定 ID 的商品資料。
+   * @param {string} productId
+   * @returns {Promise<{
+   *   success: boolean,
+   *   product: {
+   *     category: string,
+   *     content: string,
+   *     description: string,
+   *     id: string,
+   *     imageUrl: string,
+   *     imagesUrl: string[],
+   *     is_enabled: number,
+   *     origin_price: number,
+   *     price: number,
+   *     title: string,
+   *     unit: string
+   *   }
+   * }>}
+   */
+  getProductById: (productId, config = {}) => guestClient.get(`${GUEST_API_PATH}/products/${productId}`, config),
+};
 
 export const adminProductApi = {
   // 取得全部商品
