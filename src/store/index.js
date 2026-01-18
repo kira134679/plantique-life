3e5d6a73f4100238ea3dd54ee8a087fb8c7cbbe7
kira134679 <kira134679@gmail.com>
2026-01-19 01:04:07 +0800

    feat: add guestProductSlice to store

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import authReducer from '@/slice/authSlice';
 import cartReducer from '@/slice/cartSlice';
 import loadingReducer from '@/slice/loadingSlice';
 import adminProductReducer from '@/slice/product/adminProductSlice';
+import guestProductReducer from '@/slice/product/guestProductSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
@@ -12,6 +13,7 @@ const store = configureStore({
     cart: cartReducer,
     guestArticle: guestArticleReducer,
     auth: authReducer,
+    guestProduct: guestProductReducer,
   },
 });
 
