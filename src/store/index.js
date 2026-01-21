f3faeea7531640e5f4ee45bf6f3684c810ee3fe5
kira134679 <kira134679@gmail.com>
2026-01-21 22:19:19 +0800

    feat: add guestArticleSlice to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
+import guestArticleReducer from '@/slice/article/guestArticleSlice';
 import loadingReducer from '@/slice/loadingSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
   reducer: {
     loading: loadingReducer,
+    guestArticle: guestArticleReducer,
   },
 });
 
