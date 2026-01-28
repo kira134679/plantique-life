6dd386ad9888ad58de7e11305c9e42c4beaf958e
chen <75985885+CHEN507@users.noreply.github.com>
2026-01-28 23:22:43 +0800

    feat: implement URL-driven product fetching with edge case handling - 使用網址驅動取得商品列表，包含元件掛載、頁面跳轉 - 處理相關 edgeCase   - 過濾、驗證 searchParams   - 如果目前沒有商品，將頁面導回第一頁   - 如果 searchParams 的 page 值未通過驗證（非數字、非整數、非正數、大於總頁數）則顯示錯誤訊息與回第一頁按鈕

--- src/views/admin/products/Products.jsx
+++ src/views/admin/products/Products.jsx
@@ -2,13 +2,32 @@ import { useCallback, useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import toast from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router';
+import { Link, useSearchParams } from 'react-router';
 
 import { deleteProduct, fetchProducts } from '@/slice/product/adminProductSlice';
 
 import Button from '@/components/Button';
 import Pagination from '@/components/Pagination';
 import ProductRow from './components/ProductRow';
+
+function filterValidParams(params) {
+  const validKeys = ['page'];
+  const paramsEntries = Array.from(params.entries());
+  const cleanEntries = paramsEntries.filter(
+    ([key, value]) => validKeys.includes(key) && value !== null && value !== '',
+  );
+  return new URLSearchParams(cleanEntries);
+}
+
+function getPageValidationError(pageStr, total_pages) {
+  const pageNum = Number(pageStr);
+  if (!Number.isInteger(pageNum) || pageNum <= 0) {
+    return '網址頁碼不符合格式';
+  } else if (total_pages && pageNum > total_pages) {
+    return '網址頁碼超過總頁數';
+  }
+  return null;
+}
 
 function Products() {
   // --- Redux Hooks ---
@@ -19,9 +38,13 @@ function Products() {
     errors: { fetch: fetchingError },
   } = useSelector(state => state.product);
 
+  // --- Router ---
+  const [searchParams, setSearchParams] = useSearchParams();
+
   // --- Local State ---
   const [deleteId, setDeleteId] = useState(null);
   const [modalShow, setModalShow] = useState(false);
+  const [pageError, setPageError] = useState(null);
 
   // --- Event Handlers ---
   const openDeleteModal = useCallback(
@@ -37,10 +60,12 @@ function Products() {
     try {
       await dispatch(deleteProduct(deleteId)).unwrap();
       toast.success('商品刪除成功！');
-      if (current_page === 1 && products.length === 1) {
+      if (current_page === 1 && products.length === 1 && searchParams.size === 0) {
         dispatch(fetchProducts());
+      } else if (current_page === 1 && products.length === 1) {
+        setSearchParams({}, { replace: true });
       } else if (products.length === 1) {
-        dispatch(fetchProducts({ page: current_page - 1 }));
+        setSearchParams({ page: current_page - 1 }, { replace: true });
       } else {
         dispatch(fetchProducts({ page: current_page }));
       }
@@ -57,18 +82,50 @@ function Products() {
   };
 
   const handlePageChange = targetPage => {
-    dispatch(fetchProducts({ page: targetPage }));
+    setSearchParams({ page: targetPage });
   };
 
   // --- Side Effects ---
+  // 當 searchParams 有變更時，發送 API 請求，取得商品列表
   useEffect(() => {
-    dispatch(fetchProducts({ page: 1 }));
-  }, [dispatch]);
+    setPageError(null);
+
+    // 無 searchParams 時，不須驗證，直接請求第一頁資料
+    if (searchParams.size === 0) {
+      dispatch(fetchProducts());
+      return;
+    }
+
+    // API 回傳的商品列表為空時，強制清空 searchParams，觸發下一次 useEffect
+    if (total_pages === 0) {
+      setSearchParams({}, { replace: true });
+      return;
+    }
+
+    // 確認 searchParams 是否包含無效 Key
+    // 若包含，則替換為清洗後的 searchParams，觸發下一次 useEffect
+    const cleanParams = filterValidParams(searchParams);
+    if (searchParams.toString() !== cleanParams.toString()) {
+      setSearchParams(cleanParams, { replace: true });
+      return;
+    }
+
+    // 確認 searchParams 的 page 值是否符合格式
+    // 不符合即寫入 pageError state，符合則請求對應頁資料
+    const page = searchParams.get('page');
+    const pageError = page ? getPageValidationError(page, total_pages) : null;
+    setPageError(pageError);
+    if (!pageError) {
+      dispatch(fetchProducts({ page }));
+      return;
+    }
+  }, [searchParams, total_pages, dispatch, setSearchParams]);
 
   // --- View State Logic ---
+  const isInvalidUrl = !!pageError;
   const isFetchingError = !!fetchingError;
-  const isNormalProducts = !isFetchingError && total_pages > 0;
-  const hasNoProducts = !isFetchingError && total_pages === 0;
+  const isNormalProducts = !isFetchingError && !isInvalidUrl && total_pages > 0;
+  const hasNoProducts = !isFetchingError && !isInvalidUrl && total_pages === 0;
 
   return (
     <>
@@ -111,6 +168,27 @@ function Products() {
             </thead>
             {/* 表格內容*/}
             <tbody>
+              {isInvalidUrl && (
+                <tr>
+                  <td colSpan="5" className="border-0 py-6 text-center">
+                    <div className="py-8 d-flex flex-column gap-4 align-items-center">
+                      <span className="material-symbols-rounded d-block fs-1 text-danger">cancel</span>
+                      <h3 className="h5 text-neutral-400">網址錯誤</h3>
+                      <p className="text-neutral-400">錯誤說明：{pageError}</p>
+                      <Button
+                        as="button"
+                        type="button"
+                        variant="filled-primary"
+                        shape="pill"
+                        size="sm"
+                        onClick={() => setSearchParams({}, { replace: true })}
+                      >
+                        回第一頁
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              )}
               {isFetchingError && (
                 <tr>
                   <td colSpan="5" className="border-0 py-6 text-center">
