012eb12b8661747df1f987847cc149162555690d
kira134679 <kira134679@gmail.com>
2026-02-04 20:37:48 +0800

    feat: add guestNewsSlice to store

---
+++ src/slice/news/guestNewsSlice.js
@@ -0,0 +1,28 @@
+import { guestArticleApi } from '@/api';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const getNews = createAsyncThunk('guestNews/getNews', async (_, { rejectWithValue }) => {
+  try {
+    const res = await guestArticleApi.getArticles();
+    const articles = res.articles ?? [];
+    return articles.filter(article => article.tag.includes('NEWS'));
+  } catch (error) {
+    return rejectWithValue(error);
+  }
+});
+
+export const guestNewsSlice = createSlice({
+  name: 'news',
+  initialState: {
+    newsList: [],
+  },
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(getNews.fulfilled, (state, { payload }) => {
+      state.newsList = payload;
+    });
+  },
+});
+
+export const selectNewsList = state => state.guestNews.newsList;
+export default guestNewsSlice.reducer;
