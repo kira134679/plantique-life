5d488630db8d983878e0c2e301ea1d6716067707
wc-su <theblissout@gmail.com>
2026-02-09 01:04:54 +0800

    Revert "feat(admin): add BackToTopBtn to admin layout"
    This reverts commit 34cd1dfdfd4b59671c5f51d8c15d9e7797daf891.

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router';
-import BackToTopBtn from '../components/BackToTopBtn';
 import Header from '../layout/admin/Header';
 import SideMenu from '../layout/admin/SideMenu';
 
 function Admin() {
   return (
     <div className="container-xxl">
-      <BackToTopBtn />
       <Header />
       <div className="row">
         <div className="col-3">
