7e34be500031b651282fd1890bfe5009ee53936f
wc-su <theblissout@gmail.com>
2026-02-27 21:56:33 +0800

    Revert "feat: scroll to top on pathname change"
    This reverts commit aa1af1e699450f22719172c53539b75f6ac09e06.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { useEffect } from 'react';
-import { Outlet, useLocation } from 'react-router';
+import { Outlet } from 'react-router';
 import BackToTopBtn from './components/BackToTopBtn';
 import Footer from './layout/Footer';
 import Navbar from './layout/Navbar';
-import { scrollToTop } from './utils/scroll';
 
 export default function App() {
-  const location = useLocation();
-
-  // 每次路徑 (query params 不包含在內) 變化時，滾動到頂部
-  useEffect(() => {
-    scrollToTop();
-  }, [location.pathname]);
-
   return (
     <>
       <BackToTopBtn />
