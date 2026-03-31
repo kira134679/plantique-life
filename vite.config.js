8bdea6b0177ca08ac1a626623d29eb28a555e21d
kira134679 <kira134679@gmail.com>
2026-03-31 22:42:56 +0800

    feat: update build config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
+import process from 'node:process';
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  // base 的寫法:
-  // base: '/Repository 的名稱/'
-  base: '/plantique-life/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      assets: fileURLToPath(new URL('./src/assets', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // base 的寫法:
+    // base: '/Repository 的名稱/'
+    base: env.VITE_REPO,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        assets: fileURLToPath(new URL('./src/assets', import.meta.url)),
+      },
     },
-  },
-  server: {
-    open: true,
-  },
+    server: {
+      open: true,
+    },
+  };
 });
